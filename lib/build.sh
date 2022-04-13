75cb0ca589e08ef74033796db39c679bb92506b3
Paul Warnes <pwarnes@gmail.com>
2022-04-12 20:54:53 -0500

    remove old build files

--- lib/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-g++ -std=c++11 -fPIC -c -o mytest.o Test.cpp
-gcc -shared -o libtest.so mytest.o
-
-clang++ -std=c++11 -fPIC -c -o mytest-clang.o Test.cpp
-clang++ -shared -o libtest-clang.so mytest.o
-
